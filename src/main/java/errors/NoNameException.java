7fa8e95f5a2f4c6dfed8fda8a160bd6eeef067e6
kaikun213 <kaktus.w.h@gmx.de>
2016-11-28 12:02:23 +0100

    RED

---
+++ src/main/java/errors/NoNameException.java
@@ -0,0 +1,10 @@
+package main.java.errors;
+
+@SuppressWarnings("serial")
+public class NoNameException extends Exception {
+	
+	public NoNameException(String description){
+		super(description);
+	}
+
+}
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.java.errors.NoNameException;
 import main.java.model.Player;
 
 public class PlayerTest {
@@ -51,6 +52,11 @@ public class PlayerTest {
 		assertEquals("Tester",sut.getName());
 		int expected = Player.defaultCredits;
 		assertEquals(expected, sut.getCredits());
+	}
+	
+	@Test (expected=NoNameException.class)
+	public void shouldReturnErrorNoName(){
+		String actual = sut.getName();
 	}
 
 }
