7b618a3dddbe7204c83c297c3125e1169f893c1d
kaikun213 <kaktus.w.h@gmx.de>
2016-11-28 13:21:59 +0100

    GREEN

--- src/main/java/view/ConsoleView.java
+++ src/main/java/view/ConsoleView.java
@@ -17,7 +17,14 @@ public class ConsoleView implements IView{
 	}
 
 	public void showMenu() {
-		out.println();
+		out.println("-=[ Game of Chance MENU ]=-\n" +
+					"1 - Play the pick a number game\n" +
+					"2 - Play the No Match Dealer game\n" +
+					"3 - Play the Find the Ace game\n" +
+					"4 - View current high score\n" +
+					"5 - Change your user Name\n" +
+					"6 - Reset your account at 100 credits\n" +
+					"7 - Quit\n");
 	}
 
 	public Event getUserEvent() {
