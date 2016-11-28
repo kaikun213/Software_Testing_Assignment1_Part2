c8443493d4b4fc3760c93771c5d3675a4b1f9f59
kaikun213 <kaktus.w.h@gmx.de>
2016-11-28 14:30:11 +0100

    RED
    Mock static load player from XML method

--- src/test/java/GameControllerTest.java
+++ src/test/java/GameControllerTest.java
@@ -1,36 +1,55 @@
 package test.java;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import main.java.controller.GameController;
+import main.java.dao.PlayerDAO;
+import main.java.model.Player;
 import main.java.view.ConsoleView;
 import main.java.view.IView;
 
+
+@PrepareForTest({PlayerDAO.class})
+@RunWith(PowerMockRunner.class)
 public class GameControllerTest {
 	
 	GameController sut;
-
-	@Before
-	public void setUp() throws Exception {
-		
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	IView view;
 
 	@Test
 	public void shouldShowMenu() {
 		IView view = mock(ConsoleView.class);
 		sut = new GameController(view);
+		sut.play();
+		verify(view).showMenu();
+	}
+	
+	
+	@Test 
+	public void shouldTryLoadPlayer(){
+		IView view = mock(ConsoleView.class);
+		sut = new GameController(view);
+		
+		PowerMockito.mockStatic(PlayerDAO.class);
+		PowerMockito.when(PlayerDAO.jaxbXMLToObject()).thenReturn(new Player("OriginalTester"));
 		
 		sut.play();
 		
-		verify(view).showMenu();
-	}
+		PowerMockito.verifyStatic(times(1));
+		PlayerDAO.jaxbXMLToObject();
+		//PowerMockito.when(PlayerDAO.jaxbXMLToObject()).thenReturn(new Player("OriginalTester"));
 
+	}
+	
 }
