532b22c042525cbff5853170a91e2863c8cde5ae
kaikun213 <kaktus.w.h@gmx.de>
2016-11-29 13:02:10 +0100

    GREEN

--- src/main/java/dao/PlayerDAO.java
+++ src/main/java/dao/PlayerDAO.java
@@ -14,28 +14,28 @@ public class PlayerDAO {
 	private static final String File = "src/main/resources/Player.xml";
 	
     public static Player jaxbXMLToObject() {
-//        try {
-//            JAXBContext context = JAXBContext.newInstance(Player.class);
-//            Unmarshaller un = context.createUnmarshaller();
-//            Player player = (Player) un.unmarshal(new File(File));
-//            return player;
-//        } catch (JAXBException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            JAXBContext context = JAXBContext.newInstance(Player.class);
+            Unmarshaller un = context.createUnmarshaller();
+            Player player = (Player) un.unmarshal(new File(File));
+            return player;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
 
     public static void jaxbObjectToXML(Player player) {
 
-//        try {
-//            JAXBContext context = JAXBContext.newInstance(Player.class);
-//            Marshaller m = context.createMarshaller();
-//            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-//            m.marshal(player, new File(File));
-//        } catch (JAXBException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            JAXBContext context = JAXBContext.newInstance(Player.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            m.marshal(player, new File(File));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
     }
 
 }
