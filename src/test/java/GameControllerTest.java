d251513cefe601854ec7c427d6153ad4422c0626
kaikun213 <kaktus.w.h@gmx.de>
2016-11-30 13:23:13 +0100

    RED

--- src/test/java/GameControllerTest.java
+++ src/test/java/GameControllerTest.java
@@ -2,13 +2,16 @@ package test.java;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import main.java.controller.GameController;
-
+import main.java.controller.GameController.Event;
 import main.java.view.ConsoleView;
 import main.java.view.IView;
 
@@ -30,8 +33,23 @@ public class GameControllerTest {
 
 	@Test
 	public void shouldShowMenu() {
+		// mocking so it terminates
+		Mockito.when(view.getUserEvent()).thenReturn(Event.Quit);
+
 		sut.play();
 		verify(view).showMenu();
+		
+	}
+	
+	@Test
+	public void shouldQuitProgram(){
+		Mockito.when(view.getUserEvent()).thenReturn(Event.Quit);
+		
+		sut.play();
+		
+		// verify exactly called 1 time only
+		verify(view, times(1)).showMenu();
+		verify(view, times(1)).getUserEvent();
 	}
 	
 }
