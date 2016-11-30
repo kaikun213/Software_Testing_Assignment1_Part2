c1dae5bb6be4fa455974f24cc0afe600c1df4d74
kaikun213 <kaktus.w.h@gmx.de>
2016-11-30 14:45:29 +0100

    RED

--- src/test/java/GameOfChanceTest.java
+++ src/test/java/GameOfChanceTest.java
@@ -1,15 +1,25 @@
 package test.java;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.mock;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import main.java.application.GameOfChance;
+import main.java.controller.GameController;
+import main.java.dao.PlayerDAO;
+import main.java.model.Player;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GameOfChanceTest {
@@ -27,8 +37,17 @@ public class GameOfChanceTest {
 	}
 	
 	@Test
-	public void test(){
+	public void shouldPlayGameController(){
+		// initialize
+		when(sut.load()).thenReturn(new Player("Tester"));
+		doNothing().when(sut).save(any(Player.class));
+		GameController controller = mock(GameController.class);
+		when(controller.play(any(Player.class))).thenReturn(any(Player.class));
+		//run
+		sut.run(controller);
 		
+		// verify
+		verify(controller, times(1)).play(any(Player.class));
 	}
 
 
