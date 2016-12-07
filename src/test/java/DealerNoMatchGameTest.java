420ab75854be82d0645a7a8cde0d05cd67dd1a68
kaikun213 <kaktus.w.h@gmx.de>
2016-12-07 19:13:35 +0100

    RED

--- src/test/java/DealerNoMatchGameTest.java
+++ src/test/java/DealerNoMatchGameTest.java
@@ -18,7 +18,9 @@ import org.mockito.stubbing.Answer;
 
 import main.java.model.IDealerNoMatchRandomNumbersObserver;
 import main.java.model.Player;
+import main.java.model.error.NotEnoughCreditsException;
 import main.java.model.game.DealerNoMatchGame;
+import main.java.model.game.IDealerNoMatchGame;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DealerNoMatchGameTest {
@@ -32,27 +34,35 @@ public class DealerNoMatchGameTest {
 
 
 	@Test
-	public void shouldLosePlayGame() {
+	public void shouldLosePlayGame() throws NotEnoughCreditsException {
 		Mockito.when(randMock.nextInt(any(Integer.class))).thenReturn(1);
+		Mockito.doNothing().when(playerMock).decreaseCredits(any(Integer.class));
+		Mockito.doNothing().when(playerMock).increaseCredits(any(Integer.class));
 		
-		Boolean actual = sut.play();
+		Boolean actual = sut.play(50);
 		
 		assertFalse(actual);
+		Mockito.verify(playerMock, times(1)).decreaseCredits(50);
 	}
 	
 	@Test
-	public void shouldNotifyObservers(){
+	public void shouldNotifyObservers() throws NotEnoughCreditsException{
+		Mockito.doNothing().when(playerMock).decreaseCredits(any(Integer.class));
+		Mockito.doNothing().when(playerMock).increaseCredits(any(Integer.class));
 		IDealerNoMatchRandomNumbersObserver observerMock = Mockito.mock(IDealerNoMatchRandomNumbersObserver.class);
 		Mockito.doNothing().when(observerMock).randomNumberGenerated(any(Integer.class));
 		sut.addSubscriber(observerMock);
 		
-		sut.play();
+		sut.play(50);
 		
 		Mockito.verify(observerMock, times(16)).randomNumberGenerated(any(Integer.class));
 	}
 	
 	@Test
-	public void shouldWinPlayGame(){
+	public void shouldWinPlayGame() throws NotEnoughCreditsException{
+		Mockito.doNothing().when(playerMock).decreaseCredits(any(Integer.class));
+		Mockito.doNothing().when(playerMock).increaseCredits(any(Integer.class));
+
 		Mockito.when(randMock.nextInt(any(Integer.class))).thenAnswer(new Answer<Integer>() {
 		    private int count = 0;
 
@@ -61,9 +71,29 @@ public class DealerNoMatchGameTest {
 		    }
 		});	
 		
-		Boolean actual = sut.play();
+		Boolean actual = sut.play(50);
 
 		assertTrue(actual);
+		Mockito.verify(playerMock, times(1)).increaseCredits(100);
+	}
+	
+	@Test(expected = NotEnoughCreditsException.class)
+	public void shouldThrowNotEnoughCreditsException() throws NotEnoughCreditsException {
+		Mockito.doThrow(NotEnoughCreditsException.class).when(playerMock).decreaseCredits(any(Integer.class));
+		Mockito.doNothing().when(playerMock).increaseCredits(any(Integer.class));
+		Mockito.when(randMock.nextInt(any(Integer.class))).thenReturn(1);
+
+		sut.play(200);
+	}
+	
+	@Test
+	public void shouldImplementInterface() throws NotEnoughCreditsException{
+		IDealerNoMatchGame sutInterface = new DealerNoMatchGame(playerMock, randMock);
+		
+		Mockito.when(randMock.nextInt(any(Integer.class))).thenReturn(1);
+		Mockito.doNothing().when(playerMock).decreaseCredits(any(Integer.class));
+		Mockito.doNothing().when(playerMock).increaseCredits(any(Integer.class));
+		sutInterface.play(50);
 	}
 
 }
