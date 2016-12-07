e9147b75995932b8e6fa42588406ab819fd1a2d0
kaikun213 <kaktus.w.h@gmx.de>
2016-12-07 14:35:03 +0100

    RED

--- src/test/java/ConcreteGameFactoryATest.java
+++ src/test/java/ConcreteGameFactoryATest.java
@@ -5,21 +5,30 @@ import static org.junit.Assert.*;
 import java.util.Random;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.any;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.mockito.Mockito.any;
 
 import main.java.model.Player;
 import main.java.model.error.NotEnoughCreditsException;
 import main.java.model.game.AbstractGameFactory;
 import main.java.model.game.ConcreteGameFactoryA;
+import main.java.model.game.DealerNoMatchGame;
 import main.java.model.game.IPickANumberGame;
 import main.java.model.game.PickANumberGame;
 
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Player.class, DealerNoMatchGame.class, ConcreteGameFactoryA.class})
 public class ConcreteGameFactoryATest {
 
 	@Test
 	public void testPickANumberGameCreation() throws NotEnoughCreditsException {
+		// Alternative: Behavior test instead of verify with PowerMockito -> (not fully covered test)
 		Player playerMock = Mockito.mock(Player.class);
 		Random randomMock = Mockito.mock(Random.class);
 		Mockito.doNothing().when(playerMock).decreaseCredits(any(Integer.class));
@@ -27,13 +36,27 @@ public class ConcreteGameFactoryATest {
 		Mockito.when(randomMock.nextInt(any(Integer.class))).thenReturn(PickANumberGame.MIN_NUMBER);
 		IPickANumberGame expected = new PickANumberGame(playerMock, randomMock);
 		AbstractGameFactory sut = new ConcreteGameFactoryA();
-		IPickANumberGame actual = sut.getPickANumberGame(playerMock, randomMock);
+		IPickANumberGame actual = sut.getPickANumberGame(playerMock);
 
 		// test if behavior is the same
 		expected.play(PickANumberGame.MIN_NUMBER);
 		actual.play(PickANumberGame.MIN_NUMBER);
 		
 		assertEquals(expected.hasWon(), actual.hasWon());
+	}
+	
+	@Test
+	public void testNoMatchDealerGameCreation() throws Exception{
+		// Powermockito test creation
+		AbstractGameFactory sut = new ConcreteGameFactoryA();
+		Player playerMock = Mockito.mock(Player.class);
+		DealerNoMatchGame gameMock = Mockito.mock(DealerNoMatchGame.class);
+		PowerMockito.whenNew(DealerNoMatchGame.class).withAnyArguments().thenReturn(gameMock);
+		
+		sut.getDealerNoMatchGame(playerMock);
+		
+		PowerMockito.verifyNew(DealerNoMatchGame.class).withArguments(any(playerMock.getClass()), any(Random.class));
+
 	}
 
 }
